78ac3bf4c563c1aa2182f2b2aab28a59d5286369
Leandro Delinger <leandro.delinger@gmail.com>
2022-05-23 18:00:09 -0300

    aplicação

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,7 +3,7 @@ import { Button } from "./Button";
 interface SideBarProps {
   genres: Array<{
     id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
     title: string;
   }>;
   selectedGenreId: number;
@@ -13,24 +13,26 @@ interface SideBarProps {
 export function SideBar({
   genres,
   selectedGenreId,
-  buttonClickCallback
+  buttonClickCallback,
 }: SideBarProps) {
   return (
     <nav className="sidebar">
-      <span>Watch<p>Me</p></span>
+      <span>
+        Watch<p>Me</p>
+      </span>
 
       <div className="buttons-container">
-        {genres.map(genre => (
+        {genres.map((genre) => (
           <Button
             key={String(genre.id)}
             title={genre.title}
             iconName={genre.name}
+            aria-label="button-genres"
             onClick={() => buttonClickCallback(genre.id)}
             selected={selectedGenreId === genre.id}
           />
         ))}
       </div>
-
     </nav>
-  )
+  );
 }
